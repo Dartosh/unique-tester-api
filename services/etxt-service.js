9fe42ad3a57f0868228af4bda0bd95f79ea1dc6a
a.klintsou <a.klintsou@tapston.com>
2022-07-14 00:03:37 +0300

    refactor: test 24

--- services/etxt-service.js
+++ services/etxt-service.js
@@ -98,7 +98,7 @@ class EtxtAntiPlagiat {
         curlTest.setOpt(Curl.option.POST, 1);
         curlTest.setOpt(
             Curl.option.POSTFIELDS,
-            `xmlUrl=${this.localUrl}/tasks&xmlAnswerUrl=${this.localUrl}upload.php`
+            `xmlUrl=${this.localServer}/tasks&xmlAnswerUrl=${this.localServer}upload.php`
         );
         curlTest.setOpt(Curl.option.TRANSFERTEXT, 1);
         curlTest.setOpt(Curl.option.TIMEOUT, 5);
@@ -106,7 +106,7 @@ class EtxtAntiPlagiat {
         curlTest.on('end', (statusCode, data, headers) => {
             this.taskResponse = JSON.parse(data.replace('\\', ''));
 
-            console.log(`xmlUrl=${this.localServer}/tasks&xmlAnswerUrl=${this.localUrl}/upload.php`);
+            console.log(`xmlUrl=${this.localServer}/tasks&xmlAnswerUrl=${this.localServer}upload.php`);
 
             console.log(this.taskResponse);
         });
