dc16cfcb9b37f273a46a4b48cfca843cbd685e5c
a.klintsou <a.klintsou@tapston.com>
2022-08-04 01:25:07 +0300

    refactor: testing 92

--- encript.php
+++ encript.php
@@ -2,17 +2,14 @@
 
 define('CHECK_KEY', 'AAAAAAAAAAAAAAAA');
 
-$plainXml = '';
-
-echo "\n\n".file_get_contents("plainXml.txt")."\n\n";
-
+$plainXml = file_get_contents("/files/plainXml.txt");
 
 $str_length = strlen($plainXml);
 $pad_length = ($str_length % 16 == 0) ? $str_length : ($str_length +  (16 - ($str_length % 16)));
 $string = str_pad($plainXml, $pad_length, "\0", STR_PAD_RIGHT);
 
-$string = openssl_encrypt ($string, "AES-128-ECB", CHECK_KEY, (OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING));
+$string = openssl_encrypt($string, "AES-128-ECB", CHECK_KEY, (OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING));
 
-$f = @fopen('encodedXml.txt', 'w');
+$f = @fopen('/files/encodedXml.txt', 'w');
 fwrite($f, $string, strlen($string));
 fclose($f);
