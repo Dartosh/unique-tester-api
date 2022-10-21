402844e0fa5a8377945e2aec53db0cb60f3978b4
= <=>
2022-10-21 20:31:15 +0300

    refactor: urlencoded middleware

--- src/middleware/urlencoded-body.middleware.ts
+++ src/middleware/urlencoded-body.middleware.ts
@@ -5,6 +5,7 @@ import * as express from 'express';
 @Injectable()
 export class UrlencodedBodyMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    express.urlencoded({ extended: true })(req, res, next);
+    console.log('req: ', req);
+    express.urlencoded()(req, res, next);
   }
 }
