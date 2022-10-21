549c69d027c35aa5cd1051ec774895ccd39c284f
= <=>
2022-10-21 20:23:26 +0300

    refactor: test 12

--- src/middleware/urlencoded-body.middleware.ts
+++ src/middleware/urlencoded-body.middleware.ts
@@ -1,10 +1,10 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 
-import * as bodyParser from 'body-parser';
+import express from 'express';
 
 @Injectable()
 export class UrlencodedBodyMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
-    bodyParser.urlencoded()(req, res, next);
+    express.urlencoded({ extended: true })(req, res, next);
   }
 }
