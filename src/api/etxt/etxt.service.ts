922706a1a9e514ae558ac02f6fecf6a68347efe7
= <=>
2022-11-10 12:53:38 +0300

    test

--- src/api/etxt/etxt.service.ts
+++ src/api/etxt/etxt.service.ts
@@ -186,19 +186,19 @@ export class EtxtService {
   }
 
   private encryptXmlFile(xml: string): Buffer {
-    let xmlString = xml;
+    // let xmlString = xml;
 
     const cipher = createCipheriv(
       'aes-128-ecb',
       this.configService.get('E_TXT_SECRET_KEY'),
       Buffer.from([]),
-    ).setAutoPadding(false);
+    ).setAutoPadding(true);
 
-    if (xmlString.length % 16 !== 0) {
-      for (let i = 0; i < xmlString.length % 16; i++) {
-        xmlString += '\0';
-      }
-    }
+    // if (xmlString.length % 16 !== 0) {
+    //   for (let i = 0; i < xmlString.length % 16; i++) {
+    //     xmlString += '\0';
+    //   }
+    // }
 
     const encrypted = Buffer.concat([cipher.update(xml) /*, cipher.final()*/]);
 
